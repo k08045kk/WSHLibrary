eb43654d68f94c85cc8c7a0af3d2054d9d342be5
toshi <k08045kk+toshi@gmail.com>
2017-04-03 21:50:02 +0900

    文字コード、改行コード、BOM修正

--- common/EncodeUtility.js
+++ common/EncodeUtility.js
@@ -1,4 +1,4 @@
-﻿// import: ActiveXObject("Msxml2.DOMDocument");
+// import: ActiveXObject("Msxml2.DOMDocument");
 // import: ActiveXObject("System.Text.UTF8Encoding");
 // import: ActiveXObject("System.Security.Cryptography.MD5CryptoServiceProvider");
 // import: ActiveXObject("System.Security.Cryptography.SHA1CryptoServiceProvider");
